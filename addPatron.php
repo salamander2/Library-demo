772c66c0fce3235a7672b1c01abf14011148a830
Malek Hammoud <mhammoud@ip-172-31-40-221.ec2.internal>
2023-04-16 14:35:37 -0400

    Adjusted php/css, added some comments

--- addPatron.php
+++ addPatron.php
@@ -1,3 +1,88 @@
+<?php
+	/*Add Patron Page
+	This page adds a patron to the database.*/
+	require_once('common.php');
+	$db = connectToDB();
+
+	$error_message = $email = $phone = $name = $city = $prov = $address = $postal = $contact = $birthdate = "";
+//  if the form has been submited clean up all the variables and check for errors
+	if (isset($_POST['submit'])){
+		$name =  clean_input($_POST['name']);
+		$city=  clean_input($_POST['city']);
+		$prov =  clean_input($_POST['prov']);
+		$address=  clean_input($_POST['address']);
+		$postal=  clean_input($_POST['postalCode']);
+		$contact=  clean_input($_POST['contact']);
+		$birthdate=  clean_input($_POST['birthdate']);
+		$name = ucwords($name);
+		$partName= explode(" ", $name);
+		$prov = strtoupper($prov);
+		$address = ucwords($address);
+		$postal = strtoupper($postal);
+		$contactInfo = explode(",",$contact);
+		foreach ($contactInfo as $info){
+			$contact_array = str_split($info);
+			foreach ($contact_array as $char){
+				if ($char == "@"){
+					$isEmail = True;
+					break;
+				}else{
+					$isEmail =False ;
+				}
+			}
+		}
+		if($isEmail){
+			$email = $info;
+		}else{
+			$phone = $info;
+		}
+		if (strlen($phone)>20){
+			$error_message = "Try Using a valid phone number";
+		}
+		else if (strlen($email)>50){
+			$error_message = "Your email is too long, try using another one";
+		}
+		else if (sizeof($partName) != 2){
+			$error_message = "Try Using a first and last name sperated by a space";
+		}
+		else if(strlen($partName[0])>30){
+			$error_message = "Your first name appers to be too long try shortining it up";
+		}
+		else if(strlen($partName[1])>10){
+			$error_message = "Your last name appers to be too long try shortining it up";
+		}else{
+			$error_message = "";
+		}
+		if ($contact != ""){
+			$Phone = "N/A";
+			$Email= "N/A";
+			if($phone != ""){
+				$Phone = $phone;
+			}
+			if($email != ""){
+				$Email=$email;
+			}
+			$Print_contact = "Phone: ".$Phone." | Email: ". $Email;
+		}else{
+			$Print_contact = "Not specified";
+		}
+	} 
+//	if the form has been submited and there are no errors add data to the database
+	if (isset($_POST['submit']) and $error_message == ""){
+//		null if adds a NULL value to the table if the value is an emty string
+		$sql = "INSERT INTO patron (firstname,lastname, address,city, prov, postalCode,phone,email, birthdate) VALUES (?,?,?, ?, ?, ?, NULLIF(?,''), NULLIF(?,''), ?);";
+		if ($stmt = $db->prepare($sql)) {
+			$stmt->bind_param("sssssssss",$partName[0], $partName[1], $address, $city, $prov, $postal,$phone,$email,$birthdate);
+			$stmt->execute();
+			$stmt->close();
+		} else {
+			$message = 'Invalid query: ' . mysqli_error($db) . "\n";
+			$message .= 'SQL: ' . $sql;
+			die($message);
+		}
+		header("refresh:7;url=https://library.webdev.iquark.ca/" );
+	 }
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -5,6 +90,15 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="main.css"/>
+		<style>
+			.error {
+			  color:#000;
+			  font-weight:bold;
+			  padding:0;
+			  background:#F33;
+			  text-align:center;
+			}
+		</style>
 	</head>
 	<body class="is-preload">
 					<div id="main">
@@ -17,61 +111,9 @@
 							</article>
 					</div>
 		<?php
-			require_once('common.php');
-			$db = connectToDB();
-
-			$email = $phone = $name = $city = $prov = $address = $postal = $contact = $birthdate = "";
-			if (isset($_POST['submit'])){
-				$name =  clean_input($_POST['name']);
-				$city=  clean_input($_POST['city']);
-				$prov =  clean_input($_POST['prov']);
-				$address=  clean_input($_POST['address']);
-				$postal=  clean_input($_POST['postalCode']);
-				$contact=  clean_input($_POST['contact']);
-				$birthdate=  clean_input($_POST['birthdate']);
-				$name = ucwords($name);
-				$partName= explode(" ", $name);
-				$prov = strtoupper($prov);
-				$address = ucwords($address);
-				$postal = strtoupper($postal);
-				$contactInfo = explode(",",$contact);
-				foreach ($contactInfo as $info){
-					$contact_array = str_split($info);
-					foreach ($contact_array as $char){
-						if ($char == "@"){
-							$isEmail = True;
-							break;
-						}else{
-							$isEmail =False ;
-						}
-					}
-				}
-				if($isEmail){
-					$email = $info;
-				}else{
-					$phone = $info;
-				}
-				$error_message = "";
-				if (strlen($phone)>20){
-					$error_message = "Try Using a valid phone number";
-				}
-				else if (strlen($email)>50){
-					$error_message = "Your email is too long, try using another one";
-				}
-				else if (sizeof($partName) != 2){
-					$error_message = "Try Using a first and last name sperated by a space";
-				}
-				else if(strlen($partName[0])>30){
-					$error_message = "Your first name appers to be too long try shortining it up";
-				}
-				else if(strlen($partName[1])>10){
-					$error_message = "Your last name appers to be too long try shortining it up";
-				}else{
-					$error_message = "";
-				}
-			}
-
-			if (isset($_POST['submit']) and $error_message == ""){ ?>
+// 		if the form has been submited and there are no errors print the error message
+		if (isset($_POST['submit']) and $error_message == ""){
+		?>
 				<article class="post featured">
 				<div id="main">
 					<h2>Hello <?=$partName[0]?> you have beed added to the database</h2>
@@ -94,64 +136,16 @@
 					<li>
 						Birthdate: <?=$birthdate?>
 					</li>
-				<?php
-				foreach ($contactInfo as $info){
-					$contact_array = str_split($info);
-					foreach ($contact_array as $char){
-						if ($char == "@"){
-							$isEmail = True;
-							break;
-						}else{
-							$isEmail =False ;
-						}
-					}
-						if($isEmail){$email = $info;}else{$phone = $info;}
-				 }
-				if ($contact != ""){
-					if($phone != Null){?>
-						<li>	
-						Phone <?=$phone?>		
-						</li>
-					<?php }
-					if($email != Null){?>
-						<li>
-						email: <?=$email;?>		
-						</li>
-					<?php }
-				}else{?>
 					<li>
-						Contact Info: Not specified
+						Contact Info: <?=$Print_contact?>
 					</li>
-			<?php }?>
+
 				</ul>
 				<h3> You will soon be redirected back to the main page</h3>
 				</div>
 				</article>
-			<?php
-				$sql = "INSERT INTO patron (firstname,lastname, address,city, prov, postalCode,phone,email, birthdate) VALUES (?,?,?, ?, ?, ?, NULLIF(?,''), NULLIF(?,''), ?);";
-				if ($stmt = $db->prepare($sql)) {
-					$stmt->bind_param("sssssssss",$partName[0], $partName[1], $address, $city, $prov, $postal,$phone,$email,$birthdate);
-					$stmt->execute();
-					$stmt->close();
-				} else {
-					$message = 'Invalid query: ' . mysqli_error($db) . "\n";
-					$message .= 'SQL: ' . $sql;
-					die($message);
-				}
-				 header( "refresh:5;url=https://webdev.iquark.ca/~mhammoud/Library-demo/listPatron.php" );
-		 }
-		 else{ ?>
-			<style>
-				.error {
-				  color:#000;
-				  font-weight:bold;
-				  padding:4px 0 4px 4px;
-				  background:#F33;
-				  text-align:center;
-				}
-				.green { color:green; }
-			</style>
-			
+		<?php } else{ ?>
+			<!The error message!>
 			<div id="main">
 					<article class="post featured">
 						<h3 class="error">
@@ -159,6 +153,7 @@
 						</h3>
 					</article>
 			</div>
+			<!The Form!>
 			<footer id="footer" style="background-color: rgb(255, 255, 255)">
 				<section >
 					<form method="post" action="">
@@ -168,19 +163,22 @@
 								<label for="name">First and Last Name</label>
 								<input name="name" id="name" type="text" value = "<?=$name?>" maxlength = "61" required>
 							</div>
+							<br>
 							<div class="field">
 								<label for="email">Contact info (phone or email seperated by a comma): </label>
-								<input type ="name" name ="contact" id="email" value="<?=$contact?>"><sup>*Not required</sup>
+								<input type ="name" name ="contact" id="email" value="<?=$contact?>">
 							</div>
+							<br>
                             <div class="field">
 								<label for="address">Address</label>
 								<input type ="name" name ="address" id="address" value = "<?=$address?>" maxlength = "255" required>
 							</div>
+							<br>
 							<div class="field">
 								<label for="message">Birthdate</label>
 								<input type ="date" name ="birthdate" value = "<?=$birthdate?>" required>
 							</div>
-                            
+							<br>
 						</div>
 						<button type = "submit" name ="submit">SUBMIT</button>
 					</div>
@@ -207,7 +205,7 @@
 				</section>
 			</footer>
 
-		  <?php } ?>
+	 <?php } ?>
 		 
 	</body>
 </html>
